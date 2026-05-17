1432432d435c48bdc9a249570891f338bce81879
isabellaCE <isabellaemiliano14@gmail.com>
2026-05-17 03:22:01 -0300

    Updates

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./styles/raw-tokens.css";
-import "./styles/main.scss";
-
-createApp(App).use(router).mount("#app");
