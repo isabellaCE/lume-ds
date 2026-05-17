d1e8a1e6532c8e2f5b213e7f5d344756e7b0388f
Isabella Fernanda Campos Emiliano <isabellaemiliano14@gmail.com>
2026-05-17 03:07:30 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "lume-ds",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
